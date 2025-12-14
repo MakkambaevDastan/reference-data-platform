5d10e516c59414e07165dd9950a2e60acfb0449c
dasdevpro <makkambaevdastan@gmail.com>
2025-12-15 03:25:41 +0600

    fix: liquibase migration

--- server/src/main/java/com/platform/server/Application.java
+++ server/src/main/java/com/platform/server/Application.java
@@ -1,15 +1,15 @@
 package com.platform.server;
 
-import com.platform.common.entity.User;
-import com.platform.common.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = User.class)
-@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EntityScan(basePackages = {"com.platform.common", "com.platform.server"})
+@ComponentScan(basePackages = {"com.platform.common", "com.platform.server"})
+@EnableJpaRepositories(basePackages = {"com.platform.common", "com.platform.server"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
